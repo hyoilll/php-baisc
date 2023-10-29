a18fb9de0b4f52d3f4dbdd51ab5eadf81fc75016
hyoil Lee <dlgydlf123@gmail.com>
2023-10-29 13:25:22 +0900

    gettype & settype & variable variables

--- src/basic.php
+++ src/basic.php
@@ -1,9 +1,22 @@
 <?php
 
-define("TITLE", "Hello world!");
-echo TITLE. " php!";
+# gettype
+$num = 10;
+if (gettype($num) == "integer"){
+    echo "integer";
+}
+else {
+    echo "not integer";
+}
 echo "<br />";
 
-define("num", 10);
-echo var_dump(num);
+# settype
+settype($num, "string");
+echo var_dump($num);
+echo "<br />";
+
+# variable variables
+$title = "hello";
+$$title = "world";
+echo $hello;
 ?>
