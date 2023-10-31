f0e89cf2c6a1bf0ec30dd187b6fb42692753600c
hyoil Lee <dlgydlf123@gmail.com>
2023-10-31 09:46:38 +0900

    HTML FORM

--- src/basic.php
+++ src/basic.php
@@ -1,10 +1,22 @@
-<?php
+<html>
+    <body>
+        <!-- submitを押下すると、method(get)方式で、action("basic.php")に転送する。 -->
+        <form method="get" action="">
+            <label for="id">ID : </label>
+            <input type="text" name="id" />
+            <label for="pw">PW : </label>
+            <input type="text" name="pw" />
+            <input type="submit" />
+        </form>
+        <?php
 
-# $_get
+        # $_get
 
-$id = $_GET["id"];
-$pw = $_GET["pw"];
+        $id = $_GET["id"];
+        $pw = $_GET["pw"];
 
-echo $id." ".$pw;
+        echo $id." ".$pw;
 
-?>
+        ?>
+    </body>
+</html>
