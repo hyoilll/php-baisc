2bd30a2e407d87d92039147c38d3832125ab3242
hyoil Lee <dlgydlf123@gmail.com>
2023-11-03 23:57:08 +0900

    send data from html file to php file

--- src/basic.php
+++ src/basic.php
@@ -1,17 +1,4 @@
-<html>
-    <body>
-        <div>
-            <?php
-                $arr = [
-                  ["hyoil1", 10],
-                  ["hyoil2", 20],
-                  ["hyoil3", 30],  
-                ];
 
-                foreach ($arr as [$key, $value]){
-                    echo "key: {$key}, value: {$value}<br/>";
-                }
-            ?>
-        </div>
-    </body>
-</html>
+<?php
+    echo $_POST["name"]."world!!";
+?>
